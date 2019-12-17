a244c3bc3f941242874a2a5ad5e31916b6e2512a
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-12-17 14:52:39 +0700

    OK

--- application/views/peserta_didik/tambah_peserta_didik.php
+++ application/views/peserta_didik/tambah_peserta_didik.php
@@ -81,43 +81,44 @@
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                         <div class="card">
                             <form class="form-horizontal">
                                 <div class="card-body">
-                                    <h4 class="card-title">Personal Info</h4>
+                                    <h4 class="card-header" style="background:#16a085; color:#fff;">Tambah Peserta Didik</h4> <br>
+                                    <h4 class="">DATA DIRI PESERTA DIDIK</h4>
                                     <div class="form-group row">
-                                        <label for="fname" class="col-sm-3 text-right control-label col-form-label">First Name</label>
+                                        <label for="fname" class="col-sm-3  control-label col-form-label">First Name</label>
                                         <div class="col-sm-9">
                                             <input type="text" class="form-control" id="fname" placeholder="First Name Here">
                                         </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Last Name</label>
+                                        <label for="lname" class="col-sm-3  control-label col-form-label">Last Name</label>
                                         <div class="col-sm-9">
                                             <input type="text" class="form-control" id="lname" placeholder="Last Name Here">
                                         </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Password</label>
+                                        <label for="lname" class="col-sm-3  control-label col-form-label">Password</label>
                                         <div class="col-sm-9">
                                             <input type="password" class="form-control" id="lname" placeholder="Password Here">
                                         </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="email1" class="col-sm-3 text-right control-label col-form-label">Company</label>
+                                        <label for="email1" class="col-sm-3  control-label col-form-label">Company</label>
                                         <div class="col-sm-9">
                                             <input type="text" class="form-control" id="email1" placeholder="Company Name Here">
                                         </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Contact No</label>
+                                        <label for="cono1" class="col-sm-3  control-label col-form-label">Contact No</label>
                                         <div class="col-sm-9">
                                             <input type="text" class="form-control" id="cono1" placeholder="Contact No Here">
                                         </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Message</label>
+                                        <label for="cono1" class="col-sm-3  control-label col-form-label">Message</label>
                                         <div class="col-sm-9">
                                             <textarea class="form-control"></textarea>
                                         </div>
