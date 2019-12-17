1e222a1b4c68f10529a6c0ea851a89e56a8bbda1
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-12-17 21:57:18 +0700

    as

--- application/controllers/Peserta_didik.php
+++ application/controllers/Peserta_didik.php
@@ -19,4 +19,10 @@ class Peserta_didik extends CI_Controller
 
         $this->load->view('peserta_didik/tambah_peserta_didik');
     }
+
+    public function detailPeserta()
+    {
+
+        $this->load->view('peserta_didik/detail_peserta_didik');
+    }
 }
