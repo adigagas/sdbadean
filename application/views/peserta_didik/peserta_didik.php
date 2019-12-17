fb3880d12d37fe02a573ba0352464a0a15fd5707
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2019-12-17 13:18:37 +0700

    Update peserta_didik.php

--- application/views/peserta_didik/peserta_didik.php
+++ application/views/peserta_didik/peserta_didik.php
@@ -1,28 +1,5 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
-    <title>Matrix Template - The Ultimate Multipurpose admin template</title>
-    <!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>vendor/assets/extra-libs/multicheck/multicheck.css">
-    <link href="<?= base_url() ?>vendor/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
-    <link href="<?= base_url() ?>vendor/dist/css/style.min.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
-
+<?php $this->load->view('templates/header'); ?>
 <body>
     <!-- ============================================================== -->
     <!-- Preloader - style you can find in spinners.css -->
@@ -41,7 +18,7 @@
         <!-- Topbar header - style you can find in pages.scss -->
         <!-- ============================================================== -->
         <?php
-        include('templates/header.php');
+        $this->load->view('templates/header.php');
         ?>
         <!-- ============================================================== -->
         <!-- End Left Sidebar - style you can find in sidebar.scss  -->
