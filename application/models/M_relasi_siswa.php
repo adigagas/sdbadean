177d7a819b620a7a9d8ea28e5d96e65167a9bc76
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-12-19 12:20:30 +0700

    gfft

--- application/models/M_relasi_siswa.php
+++ application/models/M_relasi_siswa.php
@@ -46,4 +46,19 @@ class M_relasi_siswa extends CI_Model
         $this->id_siswa = $post['id_siswa'];
         $this->db->insert($this->_table, $this);
     }
+
+    public function getDetailSiswa($id_siswa)
+    {
+        $this->db->select('*');
+        $this->db->from('tb_relasi_siswa');
+        $this->db->join('tb_siswa', 'tb_siswa.id_siswa =tb_relasi_siswa.id_siswa');
+        $this->db->join('tb_wali', 'tb_wali.id_wali=tb_relasi_siswa.id_wali');
+        $this->db->join('tb_alamat_wali', 'tb_alamat_wali.id_alamat_wali=tb_relasi_siswa.id_alamat_wali');
+        $this->db->join('tb_alamat_ortu', 'tb_alamat_ortu.id_alamat_ortu=tb_relasi_siswa.id_alamat_ortu');
+        $this->db->join('tb_ayah', 'tb_ayah.id_ayah=tb_relasi_siswa.id_ayah');
+        $this->db->join('tb_ibu', 'tb_ibu.id_ibu=tb_relasi_siswa.id_ibu');
+        $this->db->where('tb_siswa.id_siswa', $id_siswa);
+        $query = $this->db->get()->row();
+        return $query;
+    }
 }
