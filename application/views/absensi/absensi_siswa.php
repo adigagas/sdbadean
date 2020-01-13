408c68eb304d33907e0995ecc5d869154206b2a6
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-01-14 00:45:44 +0700

    N
    N

--- application/views/absensi/absensi_siswa.php
+++ application/views/absensi/absensi_siswa.php
@@ -142,7 +142,7 @@
             <!-- footer -->
             <!-- ============================================================== -->
             <footer class="footer text-center">
-                All Rights Reserved by Matrix-admin. Designed and Developed by <a href="https://wrappixel.com">WrapPixel</a>.
+            COPYRIGHT © BIKEA TECHNOCRAFT 2019 
             </footer>
             <!-- ============================================================== -->
             <!-- End footer -->
