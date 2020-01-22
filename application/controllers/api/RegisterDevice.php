1561a17fe269865ee3b5564851dccb988bd4ae8a
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2020-01-22 20:37:04 +0700

    Api Cek Siswa

--- application/controllers/api/RegisterDevice.php
+++ application/controllers/api/RegisterDevice.php
@@ -16,13 +16,33 @@ class RegisterDevice extends REST_Controller
     }
 
 
+    function cek_post()
+    {
+
+        $nisn = $this->input->post('nisn');
+        $this->db->where('nomor_induk_sn', $nisn);
+        $data = $this->db->get('tb_siswa')->result();
+        if ($data) {
+            $this->response(array('result' => $data, 200));
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
 
     function daftar_post()
     {
+        $tes = array(
+            'nisn'           => $this->post('nisn')
+        );
+        $this->db->where('nomor_induk_sn', $tes['nisn']);
+        $cek = $this->db->get('tb_siswa')->row_array();
+        $id_siswa = $cek['id_siswa'];
         $data = array(
-            'nisn'           => $this->post('nisn'),
-            'password'          => $this->post('password'),
+            'nisn'           => $id_siswa,
+            'password'           => $this->post('password'),
             'token'           => $this->post('token')
+
         );
         $insert = $this->db->insert('tb_device', $data);
         if ($insert) {
