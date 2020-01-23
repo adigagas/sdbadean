b194440f4f2fc4b385de628811ab880180364c35
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-01-23 18:15:41 +0700

    g
    f

--- application/controllers/api/LoginOrtu.php
+++ application/controllers/api/LoginOrtu.php
@@ -12,15 +12,13 @@ class LoginOrtu extends REST_Controller
 		parent::__construct($config);
 		$this->load->database();
 
-
-
 		#Configure load model api table users
 		$this->load->model('M_device');
 	}
 
 	function index_get()
 	{
-		$a = $this->db->get('tb_device')->result();
+		$a = $this->M_device->coba()->result();
 		$this->response(array("result" => $a, 200));
 	}
 
@@ -31,9 +29,17 @@ class LoginOrtu extends REST_Controller
 		$cek = $this->M_device->get_pesan_by_id($nisn,$password);
 		if ($cek) {
 			$output['id_device'] = $cek['id_device'];
-			$output['nisn'] = $cek['nisn'];
+			$output['nisn'] = $nisn;
 			$output['password'] = $cek['password'];
 			$output['token'] = $cek['token'];
+			$output['nama_siswa'] = $cek['nama_siswa'];
+			$output['keterangan'] = $cek['keterangan'];
+			$output['tanggal_absensi'] = $cek['tanggal_absensi'];
+			$output['waktu_mulai'] = $cek['waktu_mulai'];
+			$output['waktu_selesai'] = $cek['waktu_selesai'];
+			$output['nama_pelajaran'] = $cek['nama_pelajaran'];
+			$output['nama_rombel'] = $cek['nama_rombel'];
+			$output['nama_gtk'] = $cek['nama_gtk'];
 			$this->response($output, 200);
 		} else {
 			$this->response(array('status' => 'fail', 502));
