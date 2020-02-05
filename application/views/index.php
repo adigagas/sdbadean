74cd97edfff28bf96c28484bf182f57f100abb59
adigagas <45475971+adigagas@users.noreply.github.com>
2020-02-05 07:59:28 +0700

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -117,11 +117,11 @@ if ($this->session->userdata('username') == null) {
 
                                     <div class="row">
                                         <!-- column -->
-                                        <div class="col-lg-9">
+                                        <div class="col-lg">
                                             <?php echo $this->session->flashdata('msg'); ?>
                                             <div class="row">
                                                 <?php foreach ($jadwal as $a) : ?>
-                                                    <div class="col-md-6">
+                                                    <div class="col-md-4">
                                                         <div class="flot-chart">
                                                             <div class="card text-center">
                                                                 <div class="card-header">
@@ -164,52 +164,6 @@ if ($this->session->userdata('username') == null) {
                                                         </div>
                                                     </div>
                                                 <?php endforeach; ?>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-3">
-                                            <div class="row">
-                                                <div class="col-6">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-user m-b-5 font-16"></i>
-                                                        <h5 class="m-b-0 m-t-5">2540</h5>
-                                                        <small class="font-light">Total Users</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-plus m-b-5 font-16"></i>
-                                                        <h5 class="m-b-0 m-t-5">120</h5>
-                                                        <small class="font-light">New Users</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 m-t-15">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-cart-plus m-b-5 font-16"></i>
-                                                        <h5 class="m-b-0 m-t-5">656</h5>
-                                                        <small class="font-light">Total Shop</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 m-t-15">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-tag m-b-5 font-16"></i>
-                                                        <h5 class="m-b-0 m-t-5">9540</h5>
-                                                        <small class="font-light">Total Orders</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 m-t-15">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-table m-b-5 font-16"></i>
-                                                        <h5 class="m-b-0 m-t-5">100</h5>
-                                                        <small class="font-light">Pending Orders</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 m-t-15">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-globe m-b-5 font-16"></i>
-                                                        <h5 class="m-b-0 m-t-5">8540</h5>
-                                                        <small class="font-light">Online Orders</small>
-                                                    </div>
-                                                </div>
                                             </div>
                                         </div>
                                         <!-- column -->
